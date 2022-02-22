7dbaf5edb3ef078b3b8978a613aa031a43eaa53e
Soul <top.secret.work@yandex.ru>
2022-02-23 00:48:49 +0300

    update code

--- src/MyCallable.java
+++ src/MyCallable.java
@@ -4,7 +4,7 @@ public class MyCallable implements Callable<Integer> {
     int countMessages = 0;
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call() {
         int random = (int) ((Math.random() * 10) + 1);
         for (int i = 0; i < random; i++, ++countMessages) {
             System.out.println("Привет, я " + Thread.currentThread().getName());
@@ -12,7 +12,6 @@ public class MyCallable implements Callable<Integer> {
                 Thread.sleep(1000);
             } catch (InterruptedException e) { }
         }
-        System.out.println(Thread.currentThread().getName() + ", messages: " + countMessages);
         return countMessages;
     }
 }
