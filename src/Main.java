486705eb5e419553b00111a24d74a61e3793b315
Soul <top.secret.work@yandex.ru>
2022-02-24 02:49:15 +0300

    some exp

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,41 +15,44 @@ public class Main {
         for (int i = 0; i < result.length; i++) {
             result[i] = (int) (Math.random() * Integer.MAX_VALUE);
         }
+
         long endTime = (System.nanoTime() - startTime) / 1000_000;
         System.out.println("End create array " + endTime + "ms");
         return result;
     }
 
-    public static int[] intArrayMulti(int count) throws InterruptedException {
-        int[] result = new int[count];
+    public static int[] intArrayMulti(int count) throws InterruptedException, ExecutionException {
+        int[] result = new int[10];
 
-        List<Callable<Boolean>> callableArrayList = new ArrayList<>();
+        List<Callable<int[]>> callableArrayList = new ArrayList<>();
         long startTime = System.nanoTime();
         System.out.println("Start create array");
         int forCore = count / Pool.cores;
 
-        for (int i = 0, j; i < Pool.cores; i++) {
-            int start = (i <= 0) ? 0 : i * forCore;
+        for (int i = 0; i < Pool.cores; i++) {
             if (i == Pool.cores - 1) {
                 callableArrayList.add(() -> {
-                    for (int k = start; k < forCore + (count - forCore * Pool.cores); k++) {
-                        result[k] = (int) (Math.random() * Integer.MAX_VALUE) + 1;
+                    int maxArr = forCore + (count - forCore * Pool.cores);
+                    int[] res = new int[maxArr];
+                    for (int k = 0; k < maxArr; k++) {
+                        res[k] = (int) (Math.random() * Integer.MAX_VALUE);
                     }
-                    return true;
+                    return res;
                 });
             } else {
                 callableArrayList.add(() -> {
-                    for (int n = start; n < forCore + start; n++) {
-                        result[n] = (int) (Math.random() * Integer.MAX_VALUE) + 1;
+                    int[] res = new int[forCore];
+                    for (int n = 0; n < forCore; n++) {
+                        res[n] = (int) (Math.random() * Integer.MAX_VALUE);
                     }
-                    return true;
+                    return res;
                 });
             }
         }
-        List<Future<Boolean>> futureResult = Pool.pool.invokeAll(callableArrayList);
+        List<Future<int[]>> futureResult = Pool.pool.invokeAll(callableArrayList);
 
-        for (Future<Boolean> fb : futureResult) {
-            System.out.println(fb);
+        for (Future<int[]> fb : futureResult) {
+            System.out.println(fb.get().length + ", 0: " + Arrays.stream(fb.get()).filter(a -> a == 0).sum());
         }
         long endTime = (System.nanoTime() - startTime) / 1000_000;
         System.out.println("End create array " + endTime + "ms");
@@ -62,7 +66,7 @@ public class Main {
         System.out.println("Start create array");
 
         Arrays.parallelSetAll(
-                result, i -> new Random().nextInt()
+                result, i -> (int) (Math.random() * Integer.MAX_VALUE)
         );
 
         long endTime = (System.nanoTime() - startTime) / 1000_000;
@@ -72,10 +76,11 @@ public class Main {
     }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        int[] solo = intArray(999_999_999);
+        int[] solo = intArray(299_999_999);
         System.out.println("0: " + Arrays.stream(solo).filter(a -> a == 0).count());
         solo = null;
-        int[] lam = initIntArray(999_999_999);
+
+        int[] lam = initIntArray(299_999_999);
         System.out.println("0: " + Arrays.stream(lam).filter(a -> a == 0).count());
         lam = null;
         //pool.shutdown();
--- src/Pool.java
+++ src/Pool.java
@@ -3,5 +3,6 @@ import java.util.concurrent.Executors;
 
 public class Pool {
     public static int cores = Runtime.getRuntime().availableProcessors();
+    //public static int cores = 6;
     static ExecutorService pool = Executors.newFixedThreadPool(cores);
 }
